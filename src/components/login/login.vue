c20e5a4dd7e786755f9c64e0305adb87e9ac1c48
xuweijiao <2379827011@qq.com>
2018-11-28 09:24:06 +0800

    新建登录分支-新建登录组件-配置路由

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <div>denglu</div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+// import Login from '../components/login/login.vue'
+//@会自动帮我们找到src文件夹，是webpack自动封装好的功能
+import Login from '@/components/login/login.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
