f2a07ca419ddc6796de00ce6be79cf95b4b87f55
xuweijiao <2379827011@qq.com>
2018-11-28 09:54:46 +0800

    引入el-form组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,18 +1,31 @@
 <template>
-  <div>denglu</div>
+  <div>
+    <el-form label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="primary">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name:'',
-  data(){
-   return {
-
-   }
+  name: "",
+  data() {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
