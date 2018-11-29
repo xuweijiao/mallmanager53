a30a69529aa39a236ac7811ed78d76561a3c01b5
xuweijiao <2379827011@qq.com>
2018-11-29 22:46:45 +0800

    引入侧边栏导航组件并调整样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,22 +1,94 @@
 <template>
   <el-container class="container">
     <el-header class="header">
+        <!-- 头部面包屑效果 -->
       <el-row>
         <el-col :span="4">
           <div class="grid-content bg-purple">
-              <img src="../../assets/logo.png" alt="">
+            <img src="../../assets/logo.png" alt>
           </div>
         </el-col>
         <el-col :span="18">
-            <h2 class="middle">电商后台管理系统</h2>
+          <h2 class="middle">电商后台管理系统</h2>
         </el-col>
         <el-col :span="2">
-            <a class="loginout" href="#">退出</a>
+          <a class="loginout" href="#">退出</a>
         </el-col>
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+          <!-- 侧边栏导航 -->
+        <el-menu :unique-opened="true">
+            <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -42,12 +114,12 @@ export default {};
 }
 /* 头部样式*/
 .middle {
-    text-align: center;
-    line-height: 24px;
-    color: #fff;
+  text-align: center;
+  line-height: 24px;
+  color: #fff;
 }
 .loginout {
-    line-height: 60px;
-    text-decoration: none;
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
