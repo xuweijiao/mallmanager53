c549a92645b1ee48c5a79a6f06a808ecfad83a0d
xuweijiao <2379827011@qq.com>
2018-11-30 10:22:22 +0800

    面包屑和搜索框引入组件和样式调整

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,18 +1,44 @@
 <template>
-    <div>用户</div>
+  <el-card class="box-card">
+    <!-- 1.面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 2.搜索 -->
+    <el-row class="searchRow">
+      <el-col>
+        <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success">添加用户</el-button>
+      </el-col>
+    </el-row>
+    <!-- 3.表格 -->
+    <!-- 4.分页 -->
+  </el-card>
 </template>
 
 <script>
 export default {
-  name:'',
-  data(){
-   return {
-
-   }
+  name: '',
+  data() {
+    return {
+        query: ''
+    }
   }
 }
 </script>
 
 <style>
-
+.box-card {
+  height: 100%;
+}
+.inputSearch {
+    width: 400px;
+}
+.searchRow {
+    margin-top: 30px;
+}
 </style>
