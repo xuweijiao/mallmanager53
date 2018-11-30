8c12d697854b8700d7c0044a9d6a2e3353a82022
xuweijiao <2379827011@qq.com>
2018-11-30 10:48:35 +0800

    users-表头处理

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -16,6 +16,44 @@
       </el-col>
     </el-row>
     <!-- 3.表格 -->
+     <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        label="#"
+        width="60">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="电话"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="创建时间"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="用户状态"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
     <!-- 4.分页 -->
   </el-card>
 </template>
@@ -25,7 +63,17 @@ export default {
   name: '',
   data() {
     return {
-        query: ''
+        query: '',
+        tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+        }, 
+        {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+        }]
     }
   }
 }
